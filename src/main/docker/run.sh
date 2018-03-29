667f95ce53deb01d467e50d8889bbb82913efd7b
Ilia Bochkarev <ilia_bochkarev@epam.com>
2018-03-29 17:17:28 +0300

    sh line breaks made LF

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -3,3 +3,5 @@ echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
 java -jar /usr/local/@project.artifactId@/@project.build.finalName@.jar
+
+#line breaks should be LF
